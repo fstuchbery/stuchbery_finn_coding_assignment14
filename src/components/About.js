9b5ec8925b5fddeee86c395ed229dbccd896f2c0
fstuchbery <163586465+fstuchbery@users.noreply.github.com>
2024-11-17 22:30:59 -0600

    this is the first commit. My web portfolio project.

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about">
+      <h2>Basic Information</h2>
+      <p>
+        I am a full-stack web developer in training from Winnipeg, Manitoba,
+        currently studying at Red River College. With a passion for coding, I am
+        proficient in a wide range of programming languages and frameworks,
+        including HTML, CSS, JavaScript, React, Python, Ruby, PHP, Java, and
+        more. I enjoy exploring new technologies and developing well-rounded
+        skills to build dynamic, user-friendly web applications.
+      </p>
+      <p>
+        When I’m not coding, I enjoy reading tech blogs, playing chess, and
+        contributing to open-source projects.
+      </p>
+    </section>
+  );
+}
+
+export default About;
